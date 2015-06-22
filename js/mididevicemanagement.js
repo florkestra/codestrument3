ade4caddd9e6ff3ad075d0c91566715385a8a21d
Adam Tindale <adam@adamtindale.com>
2015-06-22 10:01:41 -0400

    working with the new flocking midi management

--- js/mididevicemanagement.js
+++ js/mididevicemanagement.js
@@ -1,12 +1,82 @@
 (function(){
 
-    for (var i = 0; i < 16; i++){
-        var newdiv = document.createElement("div"); 
-        newdiv.className = "nexus";
-        newdiv.onclick = function(myvar){
-            return function(){ console.log("something " + myvar); }
-        }(i);
-        document.getElementById("midilayout").appendChild(newdiv);
-    }
+    flock.midi.requestPorts(
+        function(ports){
+            
+            if (ports.inputs.length > 0) {
+                alert("thar be ports");
+                window.flockmidi = {};
+            }
 
+            for (var i = 0; i < ports.inputs.length; i++){
+                try {
+                    mymidimappings[ports.inputs[i].name]();
+                } catch(err){
+                    console.log(err);
+                }
+            }
+        }, 
+        function(err){
+            alert(err);
+        }
+    );
+
+    var mymidimappings = {
+        QUNEO : function(){
+
+                    for (var i = 0; i < 16; i++){
+                        var newdiv = document.createElement("div"); 
+                        newdiv.className = "quneo";
+                        newdiv.onclick = function(myvar){
+                            return function(){ console.log("something " + myvar); }
+                        }(i);
+                        document.getElementById("midilayout").appendChild(newdiv);
+                    }
+
+                    window.flockmidi = flock.midi.connection({
+                        openImmediately: true,
+                        ports: {
+                            input: "QUNEO"
+                        }, 
+                        listeners: {
+                            control: function (msg) {
+                                console.log(msg);
+                            },
+                            noteOn: function (msg) {
+                                console.log(msg);
+                            }
+                        }
+                    });
+                     
+             },
+        LPD8 : function(){
+
+                    for (var i = 0; i < 8; i++){
+                        var newdiv = document.createElement("div"); 
+                        newdiv.className = "lpd8";
+                        newdiv.onclick = function(myvar){
+                            return function(){ console.log("something " + myvar); }
+                        }(i);
+                        document.getElementById("midilayout").appendChild(newdiv);
+                    }
+
+                    window.flockmidi = flock.midi.connection({
+                        openImmediately: true,
+                        ports: {
+                            input: "LPD8"
+                        }, 
+                        listeners: {
+                            control: function (msg) {
+                                console.log(msg);
+                            },
+                            noteOn: function (msg) {
+                                console.log(msg);
+                            }
+                        }
+                    });
+                     
+
+        }
+    };
+    
 }());
